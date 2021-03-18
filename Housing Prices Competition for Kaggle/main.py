6b5b0515e18ce299b2e5361ad28201d0b1eed7ed
hvkhvk <vkasikaralar@gmail.com>
2021-03-18 21:50:53 +0300

    Kaggle Housing Cont.

--- Housing Prices Competition for Kaggle/main.py
+++ Housing Prices Competition for Kaggle/main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
-from datetime import date
-from sklearn import preprocessing
 
 # Settings
 pd.set_option('display.max_rows', None)
@@ -12,180 +9,156 @@ pd.set_option('display.max_rows', None)
 train_data = pd.read_csv("Data/train.csv")
 test_data = pd.read_csv("Data/test.csv")
 
-
-def handle_missing_data(data):
-    data.drop(["Alley", "FireplaceQu", "PoolQC", "Fence", "MiscFeature"], axis="columns", inplace=True)
-    data["LotFrontage"] = data["LotFrontage"].fillna(value=round(data["LotFrontage"].mean()))
-    data.dropna(inplace=True)
-    return data
-
-def process_lot_frontage(df):
-    cut_points = [0, 50, 75, 100, 1000]
-    label_names = ["Low", "Mid", "High", "VeryHigh"]
-    df["LotFrontage"] = pd.cut(df["LotFrontage"], cut_points, labels=label_names)
+def drop_columns(df):
+    df = df.drop(["Alley", "FireplaceQu", "PoolQC", "Fence", "MiscFeature"], axis=1)
     return df
 
-def process_ages(df):
-    df["Age"] = 2021 - df["YearBuilt"]
-    df["CurrentAge"] = 2021 - df["YearRemodAdd"]
-    df["GarageAge"] = 2021 - df["GarageYrBlt"]
-    df["CurrentOwnerAge"] = 2021 - df["YrSold"]
-    df = df.drop(["YearBuilt", "YearRemodAdd", "GarageYrBlt", "YrSold"], axis=1)
+def fill_missing_data(df):
+    for col in df.columns:
+        if df[col].isna().sum() > 0:
+            # print(df[col].dtype)
+            if df[col].dtype == "object":
+                df[col] = df[col].ffill().bfill()
+            else:
+                df[col] = df[col].fillna(df[col].mean())
     return df
 
-def create_dummies(df,column_name):
-    dummies = pd.get_dummies(df[column_name], prefix=column_name)
-    df = pd.concat([df, dummies], axis=1)
-    return df
+def generate_new_columns(df):
+    df["TotalSF"] = df["1stFlrSF"] + df["2ndFlrSF"] + df["TotalBsmtSF"]
+    df['TotalPorchSF'] = (df['OpenPorchSF'] + df['3SsnPorch'] + df['EnclosedPorch'] + df['ScreenPorch'] + df['WoodDeckSF'])
 
-def drop_corr(df):
-    df = df.drop(["GarageArea", "TotRmsAbvGrd"], axis=1)
-    return df
+    df['HasPool'] = df['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
+    df['HasGarage'] = df['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
+    df['HasBsmt'] = df['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
+    df['HasFireplace'] = df['Fireplaces'].apply(lambda x: 1 if x > 0 else 0)
 
-def select_features(df):
-    df = df.drop(["MSSubClass", "TotRmsAbvGrd"], axis=1)
-    return df
-
-def separate_result(df):
-    train_data = df.drop(["SalePrice"], axis=1)
-    test_data_results = df["SalePrice"]
-
-    return train_data, test_data_results
-
-def process_categorical(df, column_name):
-    likert_scale = {'NA': -1, 'Po': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4}
-    df[column_name] = df[column_name].map(likert_scale)
-    df[column_name].astype("category")
-    return df
-
-def process_categorical_2(df, column_name):
-    likert_scale = {'NA': -1, 'No': 0, 'Mn': 1, 'Av': 2, 'Gd': 3}
-    df[column_name] = df[column_name].map(likert_scale)
-    df[column_name].astype("category")
-    return df
-
-def process_categorical_3(df, column_name):
-    likert_scale = {'NA': -1, 'Unf': 0, 'LwQ': 1, 'Rec': 2, 'BLQ': 3, 'ALQ': 4, 'GLQ': 5}
-    df[column_name] = df[column_name].map(likert_scale)
-    df[column_name].astype("category")
-    return df
-
-def process_categorical_4(df, column_name):
-    likert_scale = {'N': 0, 'Y': 1}
-    df[column_name] = df[column_name].map(likert_scale)
-    df[column_name].astype("category")
-    return df
-
-def process_categorical_5(df, column_name):
-    likert_scale = {'NA': 0, 'Unf': 1, 'RFn': 2, 'Fin': 3}
-    df[column_name] = df[column_name].map(likert_scale)
-    df[column_name].astype("category")
-    return df
-
-def process_categorical_6(df, column_name):
-    likert_scale = {'Con': 0, 'Oth': 0, 'CWD': 0, 'ConLw': 0, 'ConLI': 0, 'ConLD': 0, 'COD': 1, 'New': 2, 'WD': 3}
-    df[column_name] = df[column_name].map(likert_scale)
-    return df
-
-def train_pre_process(df):
-    df = handle_missing_data(df)
-    #df = process_lot_frontage(df)
-    df = process_ages(df)
-    df = drop_corr(df)
-
-    for col in ["OverallQual", "OverallCond"]:
-        df[col].astype("category")
-
-    for col in ["ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "KitchenQual", "GarageQual", "GarageCond", "HeatingQC"]:
-        df = process_categorical(df, col)
-
-    for col in ["BsmtExposure"]:
-        df = process_categorical_2(df, col)
-
-    for col in ["BsmtFinType1", "BsmtFinType2"]:
-        df = process_categorical_3(df, col)
-
-    for col in ["CentralAir"]:
-        df = process_categorical_4(df, col)
-
-    for col in ["GarageFinish"]:
-        df = process_categorical_5(df, col)
-
-    for col in ["SaleType"]:
-        df = process_categorical_6(df, col)
-
-    dummie_col = ["MSZoning", "Street", "LotShape", "LandContour", "Utilities", "LotConfig", "LandSlope", "Neighborhood",
-                "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd",
-                "MasVnrType", "Foundation", "Heating", "Electrical", "Functional", "GarageType", "PavedDrive", "SaleType", "SaleCondition" ]
-
-    for col in dummie_col:
-        df = create_dummies(df, col)
-    df = df.drop(dummie_col, axis=1)
+    df['Age'] = 2021 - df['YearBuilt']
 
     return df
 
-def test_pre_process(df):
-    df.drop(["Alley", "FireplaceQu", "PoolQC", "Fence", "MiscFeature"], axis="columns", inplace=True)
-    df["LotFrontage"] = df["LotFrontage"].fillna(value=round(df["LotFrontage"].mean()))
-    #df = process_lot_frontage(df)
-    df = process_ages(df)
-    df = drop_corr(df)
+train_data = drop_columns(train_data)
+test_data = drop_columns(test_data)
 
-    for col in ["OverallQual", "OverallCond"]:
-        df[col].astype("category")
+train_data = fill_missing_data(train_data)
+test_data = fill_missing_data(test_data)
 
-    for col in ["ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "KitchenQual", "GarageQual", "GarageCond", "HeatingQC"]:
-        df = process_categorical(df, col)
+train_data = generate_new_columns(train_data)
+test_data = generate_new_columns(test_data)
 
-    for col in ["BsmtExposure"]:
-        df = process_categorical_2(df, col)
 
-    for col in ["BsmtFinType1", "BsmtFinType2"]:
-        df = process_categorical_3(df, col)
+train_data_corr = train_data.corr()
+# f, ax = plt.subplots(figsize=(12, 9))
+# sns.heatmap(train_data_corr, vmax=.8, square=True)
+# plt.savefig("Plots/heatmap.png")
 
-    for col in ["CentralAir"]:
-        df = process_categorical_4(df, col)
+corr_columns = []
+for i in train_data_corr:
+    if train_data_corr[i]['SalePrice'] > 0.3:
+        innerName = train_data_corr[i].name
+        if innerName != 'SalePrice':
+            corr_columns.append(innerName)
 
-    for col in ["GarageFinish"]:
-        df = process_categorical_5(df, col)
+# print(corr_columns)
+# print(train_data.info())
 
-    for col in ["SaleType"]:
-        df = process_categorical_6(df, col)
+df = pd.get_dummies(train_data["MSZoning"])
+train_data = pd.concat([train_data, df], axis=1)
+cat_colm_train = list(df.columns)
+df = pd.get_dummies(train_data["Neighborhood"])
+train_data = pd.concat([train_data, df], axis=1)
+cat_colm_train = cat_colm_train + list(df.columns)
 
-    dummie_col = ["MSZoning", "Street", "LotShape", "LandContour", "Utilities", "LotConfig", "LandSlope", "Neighborhood",
-                "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd",
-                "MasVnrType", "Foundation", "Heating", "Electrical", "Functional", "GarageType", "PavedDrive", "SaleType", "SaleCondition" ]
+df = pd.get_dummies(test_data["MSZoning"])
+test_data = pd.concat([test_data, df], axis=1)
+cat_colm_test = list(df.columns)
+df = pd.get_dummies(test_data["Neighborhood"])
+test_data = pd.concat([test_data, df], axis=1)
+cat_colm_test = cat_colm_test + list(df.columns)
 
-    for col in dummie_col:
-        df = create_dummies(df, col)
-    df = df.drop(dummie_col, axis=1)
+col = list(set(cat_colm_train).intersection(cat_colm_test))
 
-    return df
+predict_columns = corr_columns#['LotFrontage', 'OverallQual', 'GrLivArea', 'Age', 'MasVnrArea', 'TotalSF',  'FullBath', 'HasGarage', 'TotalPorchSF', 'HasFireplace']
+predict_columns = predict_columns + col
 
-train_data = train_pre_process(train_data)
-train_data, train_data_results = separate_result(train_data)
-test_data = test_pre_process(test_data)
-test_data = test_data.dropna(axis=1)
+typ = "for"
 
-col_train = train_data.columns
-col_test = test_data.columns
-col = list(set(col_train).intersection(col_test))
+from sklearn.ensemble import RandomForestRegressor as RFR
+from sklearn.model_selection import GridSearchCV
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LogisticRegression
+if typ == "for":
+    train_x = train_data[predict_columns]
+    train_y = train_data["SalePrice"]
 
-lr = LogisticRegression()
-# score = cross_val_score(lr, train_data[col], train_data_results, cv=10 )
-# accuracy = np.mean(score)
-# print(accuracy)
-lr.fit(train_data[col], train_data_results)
-predictions = lr.predict(test_data[col])
-print(predictions)
+    # Tune parameters
+    search_params = {
+        'n_estimators'      : [23, 24, 25],
+        'max_features'      : [i for i in range(1, train_x.shape[1])],
+        'n_jobs'            : [1],
+        'min_samples_split' : [3, 4, 5, 6],
+        'max_depth'         : [22, 23, 24]
+    }
 
-holdout_ids = test_data["Id"]
-submission_df = {"Id": holdout_ids,
-                 "SalePrice": predictions}
-submission = pd.DataFrame(submission_df)
+    model = GridSearchCV(
+        RFR(),
+        search_params,
+        cv = 5,
+        n_jobs = -1,
+        verbose=True
+    )
 
-submission.to_csv("submissions/submission1.csv", index=False)
+    model.fit(train_x, train_y)
+    print(model.best_estimator_)
+
+    test_x = test_data[predict_columns]
+
+    predictions = model.predict(test_x)
+
+    holdout_ids = test_data["Id"]
+    submission_df = {"Id": holdout_ids,
+                     "SalePrice": predictions}
+    submission = pd.DataFrame(submission_df)
+
+    submission.to_csv("submissions/submission8.csv", index=False)
+
+if typ == "nn":
+    train = train_data.iloc[:900,:]
+    validation = train_data.iloc[901:,:]
+
+    train_x = train[predict_columns]
+    train_y = train["SalePrice"]
+
+    validation_x = validation[predict_columns]
+    validation_y = validation["SalePrice"]
+    print(train_x.shape)
+
+    from tensorflow import keras
+    from tensorflow.keras import layers
+
+    model = keras.Sequential([
+        layers.Dense(1024, activation='relu', input_shape=[51]),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(1),
+    ])
+    model.compile(
+        optimizer='adam',
+        loss='mse',
+    )
+
+    history = model.fit(
+        train_x, train_y,
+        validation_data=(validation_x, validation_y),
+        batch_size=256,
+        epochs=50,
+    )
+
+    test_x = test_data[predict_columns]
+    predictions = model.predict(test_x)
+    predictions = predictions.reshape(1459)
+    holdout_ids = test_data["Id"]
+    submission_df = {"Id": holdout_ids,
+                     "SalePrice": predictions}
+    submission = pd.DataFrame(submission_df)
+
+    submission.to_csv("submissions/submission7.csv", index=False)
